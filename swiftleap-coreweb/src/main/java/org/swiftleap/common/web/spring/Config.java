30490c47fe2fdcc4fc33d3bc7d20e1bc1add33b8
ruans <rm3dom@gmail.com>
2020-06-23 16:48:37 +1000

    Use boot for templating

--- swiftleap-coreweb/src/main/java/org/swiftleap/common/web/spring/Config.java
+++ swiftleap-coreweb/src/main/java/org/swiftleap/common/web/spring/Config.java
@@ -44,38 +44,4 @@ public class Config implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(securityInterceptor);
     }
-
-
-    @Bean
-    public ClassLoaderTemplateResolver templateResolver() {
-
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-
-        templateResolver.setPrefix("templates/");
-        templateResolver.setCacheable(false);
-        templateResolver.setSuffix(".html");
-        templateResolver.setCharacterEncoding("UTF-8");
-
-        return templateResolver;
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-
-        return templateEngine;
-    }
-
-    @Bean
-    public ViewResolver viewResolver() {
-
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-
-        viewResolver.setTemplateEngine(templateEngine());
-        viewResolver.setCharacterEncoding("UTF-8");
-
-        return viewResolver;
-    }
 }
